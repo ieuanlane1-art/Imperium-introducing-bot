6516a2aa5eae96834d7c18d59129e2c2737ac14e
ieuanlane1-art <ieuanlane1@hotmail.co.uk>
2026-07-01 15:04:18 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+GROUP_NAME = "Imperium"
+
+WELCOME_DELETE_TIME = 120
+
+REMINDER_TIME = 300
+
+IBS = [
+    {
+        "name": "Ieuan",
+        "username": "IeuanFX_ITA"
+    },
+    {
+        "name": "Bevs",
+        "username": "BevsFX"
+    }
+]
+
+PREFILLED_MESSAGE = (
+    "Hi! I've just joined Imperium and I'd like to get started with copy trading."
+)
