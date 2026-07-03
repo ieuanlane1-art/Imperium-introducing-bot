e7957b7de73562ba0da371597868e900096948ee
ieuanlane1-art <ieuanlane1@hotmail.co.uk>
2026-07-03 15:05:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ from handlers import (
     dashboard_command,
     panel_command,
     listibs_command,
+    addib_command,
 )
 
 
@@ -42,6 +43,7 @@ def main():
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("panel", panel_command))
     app.add_handler(CommandHandler("listibs", listibs_command))
+    app.add_handler(CommandHandler("addib", addib_command))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("chatid", chatid_command))
     app.add_handler(CommandHandler("stats", stats_command))
